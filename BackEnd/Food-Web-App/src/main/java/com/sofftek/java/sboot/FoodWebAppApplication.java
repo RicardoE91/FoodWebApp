3dd11d7911282577d484888c855692f5467797ee
Curso MTY <curso@stktrainning.com>
2020-02-07 13:11:42 -0600

    Primer Upload

---
+++ BackEnd/Food-Web-App/src/main/java/com/sofftek/java/sboot/FoodWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.sofftek.java.sboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodWebAppApplication.class, args);
+	}
+
+}
---
+++ BackEnd/Food-Web-App/src/test/java/com/sofftek/java/sboot/FoodWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofftek.java.sboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
